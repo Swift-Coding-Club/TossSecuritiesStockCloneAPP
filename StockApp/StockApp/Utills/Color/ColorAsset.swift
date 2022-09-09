cdd6a2f660db0036a9fea15c34ab430d8fca6840
Roy-wonji <shuwj81@daum.net>
2022-09-09 15:27:44 +0900

    ✨[feat]: 컬러 설정 및  컬러  변수 지정 구현

--- StockApp/StockApp/Utills/Color/ColorAsset.swift
+++ StockApp/StockApp/Utills/Color/ColorAsset.swift
@@ -10,4 +10,15 @@ import SwiftUI
 struct ColorAsset {
     static let mainColor = Color("MainColor")
     static let subColor = Color("MainColor2")
+    static let black = Color("Black")
+    static let blue = Color("Blue")
+    static let blue2 = Color("Blue2")
+    static let blue3 = Color("Blue3")
+    static let  lightgreen  = Color("LightGreen")
+    static let lightRed = Color("LightRed")
+    static let mauvepurple = Color("Mauve")
+    static let navy = Color("Navy")
+    static let navy2 = Color("Navy2")
+    static let skyblue = Color("Skyblue")
+    static let white = Color("White")
 }
