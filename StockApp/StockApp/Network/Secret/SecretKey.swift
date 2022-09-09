71f1097d2e661988785fd7bf7056a55b317ba674
Roy-wonji <shuwj81@daum.net>
2022-09-09 19:34:07 +0900

    ✨[feat]:  tabview 에 필요없는 코드 수정

--- StockApp/StockApp/Network/Secret/SecretKey.swift
+++ StockApp/StockApp/Network/Secret/SecretKey.swift
@@ -10,8 +10,4 @@ import Foundation
 struct SecretKey {
 //MARK: - 시크 릿 키의 모음
         static let BASE_KEY = "BaseUrl"
-        static let KAKAO_API_KEY = "4ace6e39f153fb9a6a1bb89ec15e9501"
-        static let NAVER_API_KEY = "pMu09tqB2CEuC2OXaylX"
-        static let NAVER_SECRET_API_KEY = "22T6mm0BCS"
-        static let GOOGLE_API_KEY = "675368339683-fnsonf1sqcf4ro260dng51jo48l15mf3.apps.googleusercontent.com "
 }
