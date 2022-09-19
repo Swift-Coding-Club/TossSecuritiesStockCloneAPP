538853135f26b4a6e6e84495cf8665cf93ade078
Roy-wonji <shuwj81@daum.net>
2022-09-19 23:53:14 +0900

    ✨[feat]: 홈 뷰 주식 , 코인 , 프로필 뷰 생성 및 구현
    * 각 폴더에 메인 파일 생성
    * 코인은  헤더 관련 작업 구현중

--- StockApp/StockApp/View/Crypto/CryptoMainView.swift
+++ StockApp/StockApp/View/Crypto/CryptoMainView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct CryptoMainView: View {
+    //MARK: - @state 및 뷰모델 선언
+    @State private var showPortfolio: Bool = false
+
+    //MARK: - 뷰를 그리는 곳
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //MARK: - 배경 색상 관련
+            Color.colorAssets.backGroundColor
+                .ignoresSafeArea()
+            
+            //MARK: - 각 뷰에 관련 된 부분
+            VStack {
+                HStack {
+                    CircleButtonView(iconName: "info")
+                    Spacer()
+                    Text("코인 시세")
+                        .font(.custom(FontAsset.mediumFont, size: 20))
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.fontColor.accentColor)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
+            }
+        }
     }
 }
 
 struct CryptoMainView_Previews: PreviewProvider {
     static var previews: some View {
-        CryptoMainView()
+        NavigationView {
+            CryptoMainView()
+                .navigationBarHidden(true)
+        }
     }
 }
---
+++ StockApp/StockApp/View/Home/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  StockApp
+//
+//  Created by 서원지 on 2022/09/19.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("메인 홈뷰 입니다")
+                .font(.title)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
