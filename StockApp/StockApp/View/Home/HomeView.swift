96302d36c2bba0184621f074c3a8bb837b0bf003
Roy-wonji <shuwj81@daum.net>
2022-10-06 00:23:56 +0900

    🪛[chore]: 컨벤션 수정

--- StockApp/StockApp/View/Home/HomeView.swift
+++ StockApp/StockApp/View/Home/HomeView.swift
@@ -24,7 +24,7 @@ struct HomeView: View {
                 Divider()
                 portfolioCoinList()
                 Spacer()
-                    .frame(height: 20)
+                    .frame(height: 20)  
             }
         }
     }
