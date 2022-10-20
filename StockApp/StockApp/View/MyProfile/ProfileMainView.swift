3c3273c17f22ca68089c4ece8c95c85424cac2c9
Roy <suhwj81@gmail.com>
2022-10-21 00:50:23 +0900

    🐛[fix]: 빌드 오류 해결

--- StockApp/StockApp/View/MyProfile/ProfileMainView.swift
+++ StockApp/StockApp/View/MyProfile/ProfileMainView.swift
@@ -25,7 +25,6 @@ struct ProfileMainView: View {
                 
                 tweetFilterBar()
                
-                
                  Spacer()
             }
             .toolbar {
