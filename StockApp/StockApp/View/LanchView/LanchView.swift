3a8822a05df976bba499ffb5e590f6796b2ff94a
Roy-wonji <shuwj81@daum.net>
2022-11-04 23:00:55 +0900

    ✨[feat]: 런치 파일 생성

---
+++ StockApp/StockApp/View/LanchView/LanchView.swift
@@ -0,0 +1,20 @@
+//
+//  LanchView.swift
+//  StockApp
+//
+//  Created by 서원지 on 2022/11/04.
+//
+
+import SwiftUI
+
+struct LanchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LanchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LanchView()
+    }
+}
