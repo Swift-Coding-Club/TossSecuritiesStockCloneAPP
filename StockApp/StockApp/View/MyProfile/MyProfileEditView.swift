08f8a83c9efa335d7d118a1710562b07596d41d2
KSYong <ericyong95@naver.com>
2022-11-22 16:12:24 +0900

    ✨[feat]: 프로필 뷰 UI 작성

--- StockApp/StockApp/View/MyProfile/MyProfileEditView.swift
+++ StockApp/StockApp/View/MyProfile/MyProfileEditView.swift
@@ -7,9 +7,116 @@
 
 import SwiftUI
 
+// MARK: - 프로필 수정 뷰
 struct MyProfileEditView: View {
+    @State var name: String = ""
+    @State var phoneNumber: String = ""
+    
     var body: some View {
-        Text("회원정보 수정 뷰 ")
+        ZStack {
+            Color.colorAssets.backGroundColor
+            
+            VStack(spacing: 40){
+                Spacer()
+                    .frame(height: 10)
+                
+                imageEditView()
+                
+                Divider()
+                
+                VStack(spacing: 20) {
+                    
+                    nameEditView()
+                    phoneNumberEditView()
+                    emailView()
+                }
+                
+                Spacer(minLength: .zero)
+            }
+        }
+        
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Text("회원정보 수정")
+                    .font(.spoqaHan(family: .Regular, size: 18))
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func imageEditView() -> some View {
+        VStack(spacing: 20) {
+            ZStack {
+                Circle()
+                    .frame(width: 120)
+                Image(systemName: "square.and.pencil")
+                    .font(.system(size: 30))
+                    .background(.white)
+                    .cornerRadius(10, corners: .allCorners)
+                    .offset(x: 40, y: 40)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func nameEditView() -> some View {
+        VStack(alignment: .leading) {
+            Text("이름")
+                .font(.spoqaHan(family: .Medium, size: 18))
+            
+            VStack {
+                TextField("홍길동", text: $name)
+                    .textFieldStyle(.plain)
+                    .autocorrectionDisabled()
+                    .keyboardType(.namePhonePad)
+                Rectangle()
+                    .frame(height:1)
+            }
+        }
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    func phoneNumberEditView() -> some View {
+        VStack(alignment: .leading) {
+            Text("전화번호")
+                .font(.spoqaHan(family: .Medium, size: 18))
+            
+            VStack {
+                TextField("010-000-0000", text: $phoneNumber)
+                    .textFieldStyle(.plain)
+                    .autocorrectionDisabled()
+                    .keyboardType(.namePhonePad)
+                Rectangle()
+                    .frame(height:1)
+                
+            }
+            .cornerRadius(4, corners: .allCorners)
+            
+        }
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    func emailView() -> some View {
+        VStack(alignment: .leading) {
+            Text("email")
+                .font(.spoqaHan(family: .Medium, size: 18))
+            
+            VStack {
+                TextField("abc123@gmail.com", text: $phoneNumber)
+                    .textFieldStyle(.plain)
+                    .autocorrectionDisabled()
+                    .keyboardType(.namePhonePad)
+                    .disabled(true)
+                Rectangle()
+                    .frame(height:1)
+                
+            }
+            .cornerRadius(4, corners: .allCorners)
+            
+        }
+        .padding(.horizontal)
     }
 }
 
