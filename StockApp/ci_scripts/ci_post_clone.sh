ba1f1520ea7f060a1aef12149638a08ae0c6fe21
Roy-wonji <shuwj81@daum.net>
2022-12-10 13:14:45 +0900

    ✨[feat]: xcode clpud  설정

--- StockApp/ci_scripts/ci_post_clone.sh
+++ StockApp/ci_scripts/ci_post_clone.sh
@@ -5,19 +5,7 @@
 #
 #  Created by 서원지 on 2022/12/10.
 #  
-cd ..
+brew install cocoapods
  
-echo ">>> SETUP ENVIRONMENT"
-echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
-echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
-export GEM_HOME=$HOME/gems
-export PATH="$GEM_HOME/bin:$PATH"
+pod install
  
-echo ">>> INSTALL BUNDLER"
-gem install bundler --install-dir $GEM_HOME
- 
-echo ">>> INSTALL DEPENDENCIES"
-bundle install
- 
-echo ">>> INSTALL PODS"
-bundle exec pod install
--- StockApp/ci_scripts/ci_post_xcodebuild.sh
+++ StockApp/ci_scripts/ci_post_xcodebuild.sh
@@ -5,19 +5,6 @@
 #
 #  Created by 서원지 on 2022/12/10.
 #  
-cd ..
+brew install cocoapods
  
-echo ">>> SETUP ENVIRONMENT"
-echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
-echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
-export GEM_HOME=$HOME/gems
-export PATH="$GEM_HOME/bin:$PATH"
- 
-echo ">>> INSTALL BUNDLER"
-gem install bundler --install-dir $GEM_HOME
- 
-echo ">>> INSTALL DEPENDENCIES"
-bundle install
- 
-echo ">>> INSTALL PODS"
-bundle exec pod install
+pod install
