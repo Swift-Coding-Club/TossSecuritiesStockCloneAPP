caa8333d7f26cab1062068dc6802c4ff89bef2bd
Roy-wonji <shuwj81@daum.net>
2022-12-10 13:04:44 +0900

    ✨[feat]: xcode clpud  설정

---
+++ StockApp/ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  StockApp
+#
+#  Created by 서원지 on 2022/12/10.
+#  
+cd ..
+ 
+echo ">>> SETUP ENVIRONMENT"
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+ 
+echo ">>> INSTALL BUNDLER"
+gem install bundler --install-dir $GEM_HOME
+ 
+echo ">>> INSTALL DEPENDENCIES"
+bundle install
+ 
+echo ">>> INSTALL PODS"
+bundle exec pod install
